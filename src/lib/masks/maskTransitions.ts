f65358f98660d024c6e3f9b225b252d69d8c709b
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-07-13 20:13:30 -0300

    refactor: improves the responsiveness of the mascara

--- src/lib/masks/maskTransitions.ts
+++ src/lib/masks/maskTransitions.ts
@@ -1,4 +1,24 @@
 import gsap from "gsap";
+import { viewport } from "$lib/stores/viewport";
+
+let width = 0,
+  height = 0;
+
+let resizeFactor = 3;
+
+viewport.subscribe((value) => {
+  ({ width, height } = value);
+
+  if (width >= 3020 && resizeFactor !== 8) {
+    resizeFactor = 8;
+  } else if (width >= 2520 && resizeFactor !== 6) {
+    resizeFactor = 6;
+  } else if (width >= 1920 && resizeFactor !== 4) {
+    resizeFactor = 4;
+  } else if (width >= 1020 && resizeFactor !== 2) {
+    resizeFactor = 2;
+  }
+});
 
 export class Mask {
   target: string;
@@ -13,7 +33,7 @@ export class Mask {
     trigger: string,
     start: string,
     end: string,
-    maskSize: string = "600vw 500vh",
+    maskSize: string = `${width * resizeFactor}px ${height * resizeFactor}px`,
     duration: number = 1
   ) {
     this.target = target;
